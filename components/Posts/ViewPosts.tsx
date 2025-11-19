39f16193e205e6d4fbeb0a551d78583350f6ded9
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-11-19 08:31:42 +0545

    New Changes

--- components/Posts/ViewPosts.tsx
+++ components/Posts/ViewPosts.tsx
@@ -1,17 +1,11 @@
 'use client'
 import { useGetPosts } from "@/api/postsApi"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
-import { Input } from "@/components/ui/input"
-import { CiSearch } from "react-icons/ci";
 
 const ViewPosts = () => {
   const {data:posts} = useGetPosts();
   return (
     <div>
-        <div className="relative">
-            <CiSearch className="absolute top-2 " />
-            <Input type="search"/>
-        </div>
         <Table>
             <TableCaption>A list of your posts.</TableCaption>
             <TableHeader>
@@ -22,7 +16,7 @@ const ViewPosts = () => {
                 </TableRow>
             </TableHeader>
             <TableBody>
-                {posts?.map((post:{id:number,title:string,body:string}) => 
+                {posts?.slice(0,10).map((post:{id:number,title:string,body:string}) => 
                     <TableRow key={post.id}>
                         <TableCell>{post.id}</TableCell>
                         <TableCell>{post.title}</TableCell>
